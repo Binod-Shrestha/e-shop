96711df421bcc25a0cbd016ebcc803ce99c672bd
Binod Shrestha <shresthabinod.ca@gmail.com>
2020-09-06 14:32:02 -0400

    Initial Commit

---
+++ e-shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  e-shop
+//
+//  Created by user on 2020-09-06.
+//  Copyright © 2020 Binod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
